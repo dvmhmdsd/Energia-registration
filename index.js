fa12c481b07ba02755aae201fd2d21e28ce74857
mohamed-saad <mahameds19999@gmail.com>
2019-06-15 21:36:59 +0200

    init the project with required files

---
+++ index.js
@@ -0,0 +1,49 @@
+// add the dependencies
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+
+// get the configurations
+const config = require("./config/db");
+
+// get the router of registeration
+const register = require('./routers/register');
+
+// init the app
+const app = express();
+
+// config the db
+mongoose.connect(config.db, { useNewUrlParser: true, useFindAndModify: false });
+// get the state of the connection
+let db = mongoose.connection;
+db.once("open", () => {
+  console.log("connected to db");
+}).on("error", err => {
+  console.log(err);
+});
+
+// set the view engine
+app.set("view engine", "pug");
+
+// init body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set the public folder
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get('/', (req, res) => {
+    res.send('Hello, world !')
+})
+
+// set the register router
+app.use('/register', register);
+
+// make the port dynamically set
+const PORT = process.env.PORT || 3000;
+// listen to the port
+app.listen(PORT, () => {
+  console.log(`app is listining on port ${PORT}`);
+});
